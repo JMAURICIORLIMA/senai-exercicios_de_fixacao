349d09251145ca9f69598b10e3cb1de76bffd42c
José Maurício <mauricio.rodriguesdelima@gmail.com>
2022-11-01 21:50:11 -0300

    Exercício 27.10.22.

--- src/aulas/senai/exercicio/desafio08/Principal.java
+++ src/aulas/senai/exercicio/desafio08/Principal.java
@@ -4,16 +4,21 @@ public class Principal {
     public static void main(String[] args) {
         CaixaDeSom caixa1 = new CaixaDeSom();
         caixa1.ligar();
-        caixa1.desligar();
         caixa1.abrirMenu();
+        caixa1.fecharMenu();
         caixa1.play();
+        caixa1.aumentarVolume();
+        caixa1.aumentarVolume();
+        caixa1.aumentarVolume();
         caixa1.aumentarVolume();
         caixa1.abrirMenu();
         caixa1.avancarMusica();
         caixa1.voltarMusica();
+        caixa1.diminuirVolume();
         caixa1.ligarMudo();
         caixa1.abrirMenu();
         caixa1.pause();
+        caixa1.fecharMenu();
         caixa1.desligar();
 
     }
